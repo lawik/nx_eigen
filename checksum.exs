f79bd52e411e683b351cf67fdf78a175925beaab
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2026-02-20 09:00:18 -0300

    chore: add checksum.exs for 0.1.1

--- checksum.exs
+++ checksum.exs
@@ -1,7 +1,4 @@
 %{
-  "nx_eigen-nif-2.17-aarch64-apple-darwin-0.1.0.tar.gz" => "sha256:19b2434ada585bdd8625a6a6efe51356155bc669a02f23d40228fc170c1c2c58",
-  "nx_eigen-nif-2.17-aarch64-arduino-uno-q-linux-gnu-0.1.0.tar.gz" => "sha256:bf7297d3e55001a334aa49b39774a4f7619ba4155d1f8e1be90154e7e3f728b6",
-  "nx_eigen-nif-2.17-aarch64-linux-gnu-0.1.0.tar.gz" => "sha256:d8d276ed2e8f5b18d2f91fbcf763faa0f9a998bded7095e973d786efced9255d",
-  "nx_eigen-nif-2.17-x86_64-apple-darwin-0.1.0.tar.gz" => "sha256:1384fd7e53f322014ff3d5fbe69e2c696cd388f93fd8979bed8d698eb220e507",
-  "nx_eigen-nif-2.17-x86_64-linux-gnu-0.1.0.tar.gz" => "sha256:e464b27433774f3e677d8fde15c570878d394b6b10e2098198271a621fbb2885",
+  "nx_eigen-nif-2.17-aarch64-apple-darwin-0.1.1.tar.gz" => "sha256:786d066780e6db1570c047b7d3d0770a28387d1bfed34f62febf376d7a785b50",
+  "nx_eigen-nif-2.17-aarch64-linux-gnu-0.1.1.tar.gz" => "sha256:6e735ff76220f83433bc02f382a91d4c21ff4ff581ede39f917c5430d5d785ac",
 }
