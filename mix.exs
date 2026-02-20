2f7b1d8b5ed7b2a687499a9a1e2cd9d1942402e1
Paulo Valente <16843419+polvalente@users.noreply.github.com>
2026-02-20 08:19:25 -0300

    update to 0.1.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NxEigen.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @source_url "https://github.com/polvalente/nx_eigen"
   @cc_template "<%= cc %>"
   @cxx_template "<%= cxx %>"
